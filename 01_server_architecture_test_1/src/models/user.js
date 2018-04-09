c776a85f4aa33e01e58d6cf095c277bb3447d145
Fégan PIERRE <fegan.pierre@gmail.com>
2018-04-09 12:38:35 +0200

    Setup routes and methods for user.js model
    GET, POST, PATCH, DELETE routes have been set and tested with
    postman.

--- 01_server_architecture_test_1/src/models/user.js
+++ 01_server_architecture_test_1/src/models/user.js
@@ -1,21 +1,55 @@
+'use strict';
+
 const db = require('./db_connection.js');
-
-// console.log(db);
-
-// db.query('SHOW DATABASES', (error, result, fields) => {
-//     if (error) throw error;
-//
-//     console.log(result);
-// });
-
-const createUsers = (request, callback) => {}
-const getUsers = (request, callback) => {}
-const putUsers = (request, callback) => {}
-const deleteUsers = (request, callback) => {}
+const createUsers = (data, callback) => {
+	// if (!data) throw new error;
+	const name = data.name ? data.name : "";
+	const mail = data.mail;
+	const password = data.password;
+	const avatar = data.avatar ? data.avatar : "";
+	const query = 'INSERT INTO User (name, mail, password, avatar)' +
+		`VALUE ("${name}", "${mail}", "${password}", "${avatar}")`;
+	db.query(query, (error, result) => {
+		callback(error, result);
+	});
+};
+const getUsers = (callback, id) => {
+	const query = !id || typeof id !== 'number' ?
+		'SELECT * FROM User' :
+		`SELECT * FROM User WHERE ID = ${id}`;
+	db.query(query, (error, result) => {
+	    if (error) throw error;
+	    callback(result);
+	});
+};
+const patchUsers = (data, callback) => {
+	const query = ['UPDATE User SET '];
+	getUsers((currentData) => {
+		let values = [];
+		const currentUser = currentData[0];
+		for (let prop in currentUser) {
+			if (data.hasOwnProperty(prop)) {
+				values.push(`${prop} = "${data[prop]}"`);
+			}
+		}
+		query.push(values.join(', ') + ` WHERE ID = ${currentUser.ID}`);
+		query = query.join('');
+		console.log(query);
+		db.query(query, (error, result) => {
+			callback(error, result);
+		});
+	}, data.id);
+};
+const deleteUsers = (id, callback) => {
+	const query = `DELETE FROM User WHERE ID = ${id}`;
+	db.query(query, (error, result) => {
+		callback(error, result);
+	});
+};
 
 module.exports = {
     create: createUsers,
     get: getUsers,
-    put: putUsers,
+    patch: patchUsers,
     delete: deleteUsers
-}
+};
--- 01_server_architecture_test_1/src/routes.js
+++ 01_server_architecture_test_1/src/routes.js
@@ -1,13 +1,47 @@
 const router = require('express').Router();
 const user = require('./models/user');
 
-router
-.get('/', (request, response) => {
-    response.send('ayyyy!!!');
+router.get('/', (request, response) => {
+    response.status(200).send('ayyyy!!!');
 })
-
 .get('/user', (request, response) => {
-    response.send('userlist!!!');
+	user.get((data) => {
+		response.status(200).send(JSON.stringify(data));
+	});
+})
+.post('/user', (request, response) => {
+	user.create(request.body, (error, data) => { // (error, data)
+		if (error) {
+			response.status(400).send("invalid parameters!");
+			return;
+		}
+		console.log(result);
+		response.status(200).send('user created');
+	});
+})
+.patch('/user/:id', (request, response) => {
+	user.patch(request.body, (error, data) => { // (error, data)
+		if (error) {
+			response.status(400).send("invalid parameters!");
+			return;
+		}
+		response.status(200).send('user modified');
+	});
+})
+.delete('/user/:id', (request, response) => {
+	user.delete(request.body.id, (error, data) => { // (error, data)
+		if (error) {
+			response.status(400).send("invalid parameters!");
+			return;
+		}
+		response.status(200).send('user deleted');
+	});
+})
+.get('/user/collection', (request, response) => {
+    response.status(200).send('user\'s collection!!!');
+})
+.get('/user/collection/song', (request, response) => {
+    response.status(200).send('collection\'s song!!!');
 })
 
 module.exports = router;
